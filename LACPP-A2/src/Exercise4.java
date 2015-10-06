124577211b09cd228aa6b7d7705283cf32aba955
David Ryman <David.ryman@hotmail.com>
2015-10-07 00:48:09 +0200

    Added fork join pool to remove crash on java 1.7

--- LACPP-A2/src/Exercise4.java
+++ LACPP-A2/src/Exercise4.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ForkJoinPool;
+
 
 public class Exercise4 {
  
@@ -6,7 +8,8 @@ public class Exercise4 {
 		int threshold = 10000;
 		QuickSortParallel quick = new QuickSortParallel(array, threshold);
 		long t1 = System.nanoTime();
-		quick.compute();
+		ForkJoinPool pool = new ForkJoinPool();
+		pool.invoke(quick);
 		//Arrays.sort(array);
 		System.out.println("Time taken: " + (System.nanoTime()-t1)/1000000000d);
 		System.out.println("Is sorted " + Quicksort.isAscending(array));
