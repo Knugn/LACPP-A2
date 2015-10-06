5e79353a9d437161ad7b55c1e57a509aaa7ed15e
David Ryman <David.ryman@hotmail.com>
2015-10-07 00:48:42 +0200

    Fixed error

--- LACPP-A2/src/Exercise2.java
+++ LACPP-A2/src/Exercise2.java
@@ -1,11 +1,12 @@
 
 public class Exercise2 {
+	
 	public static void main(String[] args) {
 		Quicksort quick = new Quicksort(40);
-		int array[] = Quicksort.createArray(10000000);
-
+		int array[] = ArrayUtils.createArray(10000000);
 		quick.sequentialQuicksort(array);
-		//boolean isAsc = Quicksort.isAscending(array);
-		//System.out.println(isAsc);
+		boolean isAsc = Quicksort.isAscending(array);
+		System.out.println(isAsc);
 	}
+	
 }
