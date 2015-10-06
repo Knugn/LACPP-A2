0b3c305296f672a730fb5a84462a5003b7b07baf
David Ryman <David.ryman@hotmail.com>
2015-10-06 20:36:53 +0200

    Fixed some warning and changed the test temporarily. Just change the parameters to alter test ranges and iterations etc.

--- LACPP-A2/src/Exercise1.java
+++ LACPP-A2/src/Exercise1.java
@@ -17,12 +17,14 @@ public class Exercise1 {
 
 	
 	public static void main(String[] args) {
-		int maxi = 40;
-		int nRuns = 10;
+		int mini = 30;
+		int maxi = 30;
+		int numi = maxi - mini + 1;
+		int nRuns = 1000;
 		ArrayList<ArrayList<Double>> times = new ArrayList<>(nRuns);
 		for (int run = 0; run < nRuns; run++) {
-			ArrayList curRunTimes = new ArrayList(maxi);
-			for (int i = 0; i < maxi; i++) {
+			ArrayList<Double> curRunTimes = new ArrayList<>(numi);
+			for (int i = mini; i <= maxi; i++) {
 				long t1 = System.nanoTime();
 				fib(i);
 				long t2 = System.nanoTime();
@@ -35,10 +37,10 @@ public class Exercise1 {
 		
 		MatlabUtil.printMatlabArray2D( times, Integer.MAX_VALUE, System.out);
 		System.out.println();
-		for (int i = 0; i < maxi; i++) {
+		for (int i = mini; i <= maxi; i++) {
 			ArrayList<Double> iTimes = new ArrayList<>(nRuns);
 			for (int run = 0; run < nRuns; run++) {
-				iTimes.add(times.get(run).get(i));
+				iTimes.add(times.get(run).get(i - mini));
 			}
 			System.out.println("Fib " + i + " computation time (ms) statistics over " + nRuns + " runs.");
 			OneVarStatistics stats = new OneVarStatistics(iTimes);
