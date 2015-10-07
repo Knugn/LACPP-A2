a3adb05e0e84f6f1d152fdc817eeff5d726ac89d
Anders <Anders@DESKTOP-59G7R5N>
2015-10-07 20:27:02 +0200

    Added additional information to exe4

--- LACPP-A2/src/Exercise4.java
+++ LACPP-A2/src/Exercise4.java
@@ -2,18 +2,30 @@ import java.util.concurrent.ForkJoinPool;
 
 
 public class Exercise4 {
- 
+
 	public static void main(String[] args) {
-		int array[] = ArrayUtils.createArray(100000000);
-		int threshold = 10000;
-		QuickSortParallel quick = new QuickSortParallel(array, threshold);
-		long t1 = System.nanoTime();
-		ForkJoinPool pool = new ForkJoinPool();
-		pool.invoke(quick);
-		//Arrays.sort(array);
-		System.out.println("Time taken: " + (System.nanoTime()-t1)/1000000000d);
-		System.out.println("Is sorted " + Quicksort.isAscending(array));
+
+
+		int maxThreshold = 16384;
+		int startSize = 65536;
+		int maxSize = 67108864;
+
+		for (int size = startSize; size < maxSize; size*=2)
+		for (int threshold = 2; threshold < maxThreshold; threshold*=2){
+			for (int i = 0; i < 5; i++){
+				int array[] = ArrayUtils.createArray(size);
+				QuickSortParallel quick = new QuickSortParallel(array, threshold);
+				long t1 = System.nanoTime();
+				ForkJoinPool pool = new ForkJoinPool();
+				pool.invoke(quick);
+				long endTime = System.nanoTime();
+
+				System.out.println("Threshold is: " + threshold + " size is: " + size);
+				System.out.println("Time taken: " + (endTime-t1)/1000000d);
+
+			}
+		}
 
 	}
-	
+
 }
