d44e59dba7715ad8e204770edd346819063ec330
Anders <Anders@DESKTOP-59G7R5N>
2015-10-07 18:50:45 +0200

    Added so exersize 3 runes multiple times and saves the result

--- LACPP-A2/src/Exercise3.java
+++ LACPP-A2/src/Exercise3.java
@@ -1,29 +1,37 @@
+import java.util.LinkedList;
+
 
 public class Exercise3 {
-	
+
 	public static void main(String[] args) {
 		Quicksort quicksort = new Quicksort();
 		int Ns[] = {1,2,4,8,16,32,64,128};
-		int size = 2000000;
+		int size = 500000;
+		LinkedList<LinkedList<Double>> allTimeTaken = new LinkedList<LinkedList<Double>>();
 		for (int sizeIndex = 0; sizeIndex < Ns.length; sizeIndex++){
-			
-			Thread [] threds = new Thread[Ns[sizeIndex]];
-			int arrays[][] = ArrayUtils.createArrays(Ns[sizeIndex],size);
-			
-			long time = System.nanoTime();
-			for (int i = 0; i < arrays.length; i++){
-				threds[i] = quicksort.threadedQuicksort(arrays[i]);
-			}
-			for (int i = 0; i < arrays.length; i++){
-				try {
-					threds[i].join();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+			 LinkedList<Double> timeTaken = new LinkedList<Double>();
+			for (int n = 0; n < 5; n++){
+				Thread [] threds = new Thread[Ns[sizeIndex]];
+				int arrays[][] = ArrayUtils.createArrays(Ns[sizeIndex],size);
+
+				long time = System.nanoTime();
+				for (int i = 0; i < arrays.length; i++){
+					threds[i] = quicksort.threadedQuicksort(arrays[i]);
 				}
+				for (int i = 0; i < arrays.length; i++){
+					try {
+						threds[i].join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				long endTime = System.nanoTime();
+				double totalTimeTaken = (endTime - time)/1000000d;
+				timeTaken.add(totalTimeTaken);
+				System.out.println("Nummber of threads: " + Ns[sizeIndex]);
+				System.out.println("time taken is: " + totalTimeTaken);
 			}
-			
-			System.out.println("Nummber of threads: " + Ns[sizeIndex]);
-			System.out.println("time taken is: " + (System.nanoTime() - time)/1000000d);
+			allTimeTaken.add(timeTaken);
 		}
 
 	}
