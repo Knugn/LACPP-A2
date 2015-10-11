5f13930ceec09ac7e15eaf3521fe61b39df4ea9b
David Ryman <David.ryman@hotmail.com>
2015-10-11 15:13:53 +0200

    Updated exercise 1 with a reasonable test range and useful data printing.

--- LACPP-A2/src/Exercise1.java
+++ LACPP-A2/src/Exercise1.java
@@ -1,4 +1,6 @@
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Exercise1 {
@@ -14,14 +16,12 @@ public class Exercise1 {
 		}
 	}
 	
-
-	
 	public static void main(String[] args) {
-		int mini = 30;
-		int maxi = 30;
+		int mini = 12;
+		int maxi = 32;
 		int numi = maxi - mini + 1;
 		int nRuns = 10;
-		ArrayList<ArrayList<Double>> times = new ArrayList<>(nRuns);
+		List<List<Double>> allRunTimes = new ArrayList<>(nRuns);
 		for (int run = 0; run < nRuns; run++) {
 			ArrayList<Double> curRunTimes = new ArrayList<>(numi);
 			for (int i = mini; i <= maxi; i++) {
@@ -31,21 +31,39 @@ public class Exercise1 {
 				double dt = (t2-t1) / 1000000d;
 				curRunTimes.add(dt);
 			}
-			times.add(curRunTimes);
+			allRunTimes.add(curRunTimes);
 			
 		}
+		ArrayList<ArrayList<Double>> allITimes = new ArrayList<>(nRuns);
 		
-		MatlabUtil.printMatlabArray2D( times, Integer.MAX_VALUE, System.out);
-		System.out.println();
+		//MatlabUtil.printMatlabArray2D( allRunTimes, Integer.MAX_VALUE, System.out);
+		//System.out.println();
 		for (int i = mini; i <= maxi; i++) {
 			ArrayList<Double> iTimes = new ArrayList<>(nRuns);
 			for (int run = 0; run < nRuns; run++) {
-				iTimes.add(times.get(run).get(i - mini));
+				iTimes.add(allRunTimes.get(run).get(i - mini));
 			}
+			allITimes.add(iTimes);
+			/*
 			System.out.println("Fib " + i + " computation time (ms) statistics over " + nRuns + " runs.");
 			OneVarStatistics stats = new OneVarStatistics(iTimes);
-			System.out.println(stats);
+			System.out.println(stats);*/
 		}
+		
+		List<OneVarStatistics> statList = OneVarStatUtils.getStatList(allITimes);
+		String[] keyStatNamesArr = {"Mean", "Max", "Q3", "Median", "Q1", "Min"};
+		List<String> keyStatNamesList = new ArrayList<>();
+		for (String s : keyStatNamesArr) {
+			keyStatNamesList.add(s);
+		}
+		List<List<Double>> keyStatLists = OneVarStatUtils.getKeyLists(statList, keyStatNamesList);
+		
+		PrintStream out = System.out;
+		out.println();
+		out.print("Key statisical value list in order: ");
+		MatlabUtil.printMatlabArray(keyStatNamesList, Integer.MAX_VALUE, out);
+		out.println();
+		MatlabUtil.printMatlabArray2D(keyStatLists, Integer.MAX_VALUE, out);
 	}
 	
 }
